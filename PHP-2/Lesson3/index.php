7e4d190adc0c509fb9f130b66c93ccdd5868a9fa
k0c0u <k0c0u@mail.ru>
2018-11-20 14:36:19 +0300

    add

---
+++ PHP-2/Lesson3/index.php
@@ -0,0 +1,34 @@
+<?php
+
+CONST PHOTO_PATH = 'data/photo';
+CONST PHOTO_SMALL_PATH = 'data/photo_small';
+
+// подгружаем и активируем авто-загрузчик Twig-а
+require 'vendor/autoload.php';
+
+try {
+  // указывае где хранятся шаблоны
+  $loader = new Twig_Loader_Filesystem('templates');
+  
+  // инициализируем Twig
+  $twig = new Twig_Environment($loader);
+  
+  // подгружаем шаблон
+  $template = $twig->loadTemplate('index.tmpl');
+  
+  // Получаем список фотографий 
+  $photos_in_dir = array_slice(scandir(PHOTO_PATH), 2);
+
+  // передаём в шаблон переменные и значения
+  // выводим сформированное содержание
+  //echo print_r($photos_in_dir);
+  echo $template->render(array(
+            'title' => 'Список фотографий альбома',
+            'path_to_photo_small' => PHOTO_SMALL_PATH,
+            'photos' => $photos_in_dir
+            ));
+  
+} catch (Exception $e) {
+  die ('ERROR: ' . $e->getMessage());
+}
+?>
---
+++ PHP-2/app/View/Page.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mirra
+ * Date: 19.11.2018
+ * Time: 11:26
+ */
+
+namespace App\View;
+
+
+class Page
+{
+
+}
